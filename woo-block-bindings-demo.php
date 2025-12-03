2138ec0e56e4cd58a36c2d7de05ef8bd345c3cba
Brian Coords <bacoords@gmail.com>
2025-12-02 18:30:00 -0800

    updates plugin author and desctiption

--- woo-block-bindings-demo.php
+++ woo-block-bindings-demo.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * Plugin Name:       Woo Block Bindings Demo
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       Using Block Bindings to pull in Product Category Image
  * Version:           0.1.0
- * Requires at least: 6.7
+ * Requires at least: 6.9
  * Requires PHP:      7.4
- * Author:            The WordPress Contributors
+ * Author:            bacoords
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       woo-block-bindings-demo
  *
- * @package CreateBlock
+ * @package WooBlockBindingsDemo
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
